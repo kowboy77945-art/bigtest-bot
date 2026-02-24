6e549266265f9493a490a070ea9b294946c43416
kowboy77945-art <kowboy77945@gmail.com>
2026-02-24 19:51:36 +0200

    Update bot token retrieval and startup message

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+# Railway сам подставит токен из настроек Variables
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("Мой создатель цитру")
+
+async def main():
+    print("Бот запущен и готов к работе!")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+  
